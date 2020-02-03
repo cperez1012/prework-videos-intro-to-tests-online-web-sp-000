2019796fadb8cd2ec235e88f1ab6a9a0b5de4b4e
Christian Perez <perezchristian1012@gmail.com>
2020-02-03 18:16:25 +0000

    Automatically backed up by Learn

--- conversions.rb
+++ conversions.rb
@@ -1,5 +1,5 @@
 + Awesome, so we now have this automated test suite - and I can use this to make sure that, if we make any changes to the code, that the method still does what it's supposed to do. Let's change that method now:
-  ```ruby
+
 GRAMS_PER_OUNCE = 28.3495
 def ounces_to_grams(ounces)
   ounces.to_f * GRAMS_PER_OUNCE
